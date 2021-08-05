6ca7b48bd0969b825f62ab6fcfaaea81deba5c2b
bboatwright <68251659+bboatwright@users.noreply.github.com>
2021-08-05 11:58:36 -0400

    Update pc_align.sh

--- pc_align.sh
+++ pc_align.sh
@@ -5,6 +5,7 @@ for tifstring in $tiflist
 do
 tifname=${tifstring%-DEM-adj.tif}
 pc_align --max-displacement 1000 --save-transformed-source-points mola128_stereoclip.tif "$tifname"-DEM-adj.tif -o "$tifname"
+# mola128_stereoclip.tif is what I named my MOLA tile, but you can name it whatever you like
 point2dem --tr 20 "$tifname"-trans_source.tif
 c=$(($c+1))
 done
