d5b568cf06c4e244edc00d64b4ad3708a8cdf89c
bboatwright <68251659+bboatwright@users.noreply.github.com>
2021-08-05 11:39:12 -0400

    Update pc_align.sh

--- pc_align.sh
+++ pc_align.sh
@@ -4,8 +4,8 @@ c=1
 for tifstring in $tiflist
 do
 tifname=${tifstring%-DEM-adj.tif}
-~/StereoPipeline-2.6.2-2019-06-17-x86_64-Linux/bin/pc_align --max-displacement 1000 --save-transformed-source-points mola128_stereoclip.tif "$tifname"-DEM-adj.tif -o "$tifname"
-~/StereoPipeline-2.6.2-2019-06-17-x86_64-Linux/bin/point2dem --tr 25 "$tifname"-trans_source.tif
+pc_align --max-displacement 1000 --save-transformed-source-points mola128_stereoclip.tif "$tifname"-DEM-adj.tif -o "$tifname"
+point2dem --tr 20 "$tifname"-trans_source.tif
 c=$(($c+1))
 done
 
